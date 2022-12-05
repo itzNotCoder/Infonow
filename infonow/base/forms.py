386a8a71374e0e4cbff574857dcac22ebc0fe796
Abdul Salam <106696195+silentProgrammer7@users.noreply.github.com>
2022-12-05 20:35:01 +0500

    Add files via upload

---
+++ infonow/base/forms.py
@@ -0,0 +1,7 @@
+# from django import forms
+
+# class ContactForm(forms.Form):
+#     yourname = forms.CharField(max_length=100, label='Your Name')
+#     email = forms.EmailField(required=True, label='Your Email')
+#     subject = forms.CharField(max_length=100)
+#     message = forms.CharField(widget=forms.Textarea)
---
+++ infonow/base/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('contactus', views.contactus, name='contactus'),
+    path('news', views.news, name='news'),
+    path('espn-news', views.espn_news, name='espn_news'),
+    path('ign-news', views.ign_news, name='ign_news'),
+    path('cnn-news', views.cnn_news, name='cnn_news'),
+    path('bbc-news', views.bbc_news, name='bbc_news'),
+    path('theverge-news', views.theverge_news, name='theverge_news'),
+    path('techcrunch-news', views.techcrunch_news, name='techcrunch_news'),
+    # path('football-news', views.football_news, name='football_news'),
+    # path('basketball-news', views.basketball_news, name='basketball_news'),
+    # path('baseball-news', views.baseball_news, name='baseball_news'),
+    # path('cricket-news', views.cricket_news, name='cricket_news'),
+    # path('bitcoin-news', views.bitcoin_news, name='bitcoin_news'),
+    # path('coronavirus-news', views.coronavirus_news, name='coronavirus_news'),
+]
